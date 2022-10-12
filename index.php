1329c12fe4a7424a6c5db9c56657986845275d61
kiiii8r <mg-e.s90@outlook.jp>
2022-10-13 01:41:11 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php include "common/header.html" ?>
+
+<section id="toppage">
+    <header class="header">
+        <h1 class="title"><span>Vestibulum mauris</span></h1>
+    </header>
+    <div class="post innerS">
+        <p>ちかごろ世間で日本歴史の科学的研究ということがしきりに叫ばれている。科学的研究というのが近代史学の学問的方法による研究という意義であるならば、これは史学の学徒の間においては一般に行われていることであるから、今さらこと新しくいうには及ばないはずである。上にいったようなことがらについては、曖昧あいまいな態度をとり、または真実でない知識を強いて注入していたことも、明かな事実である。
+        </p>
+    </div>
+</section>
+
+<iframe
+  src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%250A%253Chead%253E%250A%2520%2520%2520%2520%253Cmeta%2520charset%253D%2522UTF-8%2522%2520%252F%253E%250A%2520%2520%2520%2520%253Cmeta%2520name%253D%2522viewport%2522%2520content%253D%2522width%253Ddevice-width%2522%2520%252F%253E%250A%2520%2520%2520%2520%253Cmeta%2520name%253D%2522description%2522%2520content%253D%2522%25E3%2583%2595%25E3%2582%25A1%25E3%2583%2583%25E3%2582%25B7%25E3%2583%25A7%25E3%2583%25B3%25E3%2582%2592%25E9%2580%259A%25E3%2581%2598%25E3%2581%25A6%25E6%2598%258E%25E3%2582%258B%25E3%2581%258F%25E8%25B1%258A%25E3%2581%258B%25E3%2581%25AA%25E7%2594%259F%25E6%25B4%25BB%25E3%2582%2592%2522%2520%252F%253E%250A%2520%2520%2520%2520%253Ctitle%253EMy%2520fashion%2520%257C%2520%25E3%2583%2595%25E3%2582%25A1%25E3%2583%2583%25E3%2582%25B7%25E3%2583%25A7%25E3%2583%25B3%25E3%2582%2592%25E9%2580%259A%25E3%2581%2598%25E3%2581%25A6%25E6%2598%258E%25E3%2582%258B%25E3%2581%258F%25E8%25B1%258A%25E3%2581%258B%25E3%2581%25AA%25E7%2594%259F%25E6%25B4%25BB%25E3%2582%2592%253C%252Ftitle%253E%250A%2520%2520%2520%2520%253Clink%2520rel%253D%2522stylesheet%2522%2520type%253D%2522text%252Fcss%2522%2520media%253D%2522all%2522%2520href%253D%2522css%252Fstyle.css%2522%2520%252F%253E%250A%2520%2520%2520%2520%253Cscript%2520src%253D%2522js%252Fscript.js%2522%253E%253C%252Fscript%253E%250A%253C%252Fhead%253E"
+  style="width: 808px; height: 348px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+
+<ul class="col3">
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap1-280x280.jpg" /></p>
+                <h3><span>枝の上に緑の点を打って</span></h3>
+                <p>初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って遠く見ると匂いやかに煙って &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap2-280x280.jpg" /></p>
+                <h3><span>読我書屋の狭い庭から</span></h3>
+                <p>五月の日に光るかなめの若葉、柿の若葉。読我書屋の狭い庭から、段々遠い林に眼をやって、更にあたりの景色に憧れ、ふ &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap3-280x280.jpg" /></p>
+                <h3><span>そういう店にそれは急な下り坂になって</span></h3>
+                <p>樹の多い山の手の初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って、遠く見る &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap4-280x280.jpg" /></p>
+                <h3><span>五月の日に光るかなめの若葉</span></h3>
+                <p>読我書屋の狭い庭から、段々遠い林に眼をやって、更にあたりの景色に憧れ、ふら家を出るのもこの頃である。明るい日は &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap5-280x280.jpg" /></p>
+                <h3><span>そういう店にそれは急な下り坂になって小家がちの谷の向う</span></h3>
+                <p>樹の多い山の手の初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って、遠く見る &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap6-280x280.jpg" /></p>
+                <h3><span>家を出るのもこの頃である</span></h3>
+                <p>樹の多い山の手の初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って、遠く見る &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap7-280x280.jpg" /></p>
+                <h3><span>段々遠い林に眼をやって</span></h3>
+                <p>樹の多い山の手の初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って、遠く見る &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap8-280x280.jpg" /></p>
+                <h3><span>頓狂に畳を叩く音</span></h3>
+                <p>読我書屋の狭い庭から、段々遠い林に眼をやって、更にあたりの景色に憧れ、ふら家を出るのもこの頃である。明るい日は &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+    <li>
+        <a href="subpage.html">
+            <div>
+                <p class="thumb"><img width="280" height="280" src="images/cap9-280x280.jpg" /></p>
+                <h3><span>新商品入荷情報</span></h3>
+                <p>樹の多い山の手の初夏の景色ほど美しいものはない。始めは樹々の若芽が、黒々とした枝の上に緑の点を打って、遠く見る &#8230;</p>
+                <p class="date">2017/01/06</p>
+            </div>
+        </a>
+    </li>
+</ul>
+
+<?php include "common/footer.html" ?>
